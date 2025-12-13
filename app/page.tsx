c96b1509350d1a44e65e0a99d34c2957dc070778
Mohd Zaid <shaikhzaid3131@gmail.com>
2025-12-13 15:27:59 +0530

    Project structure setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-3xl font-bold text-blue-600">
         EduPlus – Micro Learning Platform
       </h1>
     </main>
